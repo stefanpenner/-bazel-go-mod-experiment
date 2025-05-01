fd32f564b56d5f55e7cdd69706899997e15af2f5
Stefan Penner <stefan.penner@gmail.com>
2025-05-01 16:58:02 -0600

    introduce inter module dependencies

--- mod_a/go.mod
+++ mod_a/go.mod
@@ -1,3 +1,7 @@
 module github.com/stefanpenner/-bazel-go-mod-experiment/mod_a
 
 go 1.23.3
+
+require github.com/stefanpenner/-bazel-go-mod-experiment/mod_b v0.0.0
+
+replace github.com/stefanpenner/-bazel-go-mod-experiment/mod_b => ../mod_b/
--- mod_a/main.go
+++ mod_a/main.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 
 	"github.com/stefanpenner/-bazel-go-mod-experiment/mod_a/foo"
+	"github.com/stefanpenner/-bazel-go-mod-experiment/mod_b"
 )
 
 func main() {
-	fmt.Printf("Hello, %s!", foo.Hello())
+	fmt.Printf("%s, World!\n", foo.Hello())
+	fmt.Printf("%s, World!\n", mod_b.Hello())
 }
