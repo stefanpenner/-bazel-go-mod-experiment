005f4119e6f1fd0fecebd17eae0b586e5f7e7c5f
Stefan Penner <stefan.penner@gmail.com>
2025-05-02 14:35:27 -0600

    some notes

--- go_mod_tool/run.go
+++ go_mod_tool/run.go
@@ -22,12 +22,15 @@ func run(cfg Config) error {
 		return fmt.Errorf("failed to parse status file %s: %w", cfg.VolatileStatusFile, err)
 	}
 
+	// TODO: rather then just 1 volatile version, we need to support multiple, one per module
 	version, has_version := status["VOLATILE_VERSION"]
 	// Default VOLATILE_VERSION to __unversioned__ if not set
 	if !has_version {
 		version = "__unversioned__"
 	}
 	moduleDir := cfg.ModulePath + "@" + version
+
+	// TODO: now look at the go.mod, and update versions based on the version set in the status file
 
 	if err := addFileToZip(zw, cfg.GoMod, filepath.Join(moduleDir, "go.mod")); err != nil {
 		return fmt.Errorf("failed to add go.mod to zip: %w", err)
