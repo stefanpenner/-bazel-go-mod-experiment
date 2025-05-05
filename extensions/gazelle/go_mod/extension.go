55c26c237007b6c8b676e5e9dcf163c747b0f882
Stefan Penner <stefan.penner@gmail.com>
2025-05-05 16:01:35 -0600

    some go_mod rules are injected, others are not. Seems like ones in nested dirs work? Very odd

--- extensions/gazelle/go_mod/extension.go
+++ extensions/gazelle/go_mod/extension.go
@@ -3,7 +3,6 @@ package go_mod
 import (
 	"flag"
 	"fmt"
-	"strings"
 
 	"github.com/bazelbuild/bazel-gazelle/config"
 
@@ -42,7 +41,7 @@ func (*GoMod) Kinds() map[string]rule.KindInfo {
 func (*GoMod) Loads() []rule.LoadInfo {
 	return []rule.LoadInfo{
 		{
-			Name:    "@//rules:go_mod.bzl",
+			Name:    "@bazel-go-mod-experiment//rules:go_mod.bzl",
 			Symbols: []string{"go_mod"},
 		},
 	}
@@ -50,34 +49,38 @@ func (*GoMod) Loads() []rule.LoadInfo {
 
 // GenerateRules generates rules for go.mod files in a directory.
 func (*GoMod) GenerateRules(args language.GenerateArgs) language.GenerateResult {
-	var srcs []string
+	var hasGoMod bool
 	// TODO: this actually needs to work properly:
 	// - files in child directories of go.mod should be included if they match the go.mod inclusion pattern
 	// - subpackage files need to be included (assuming they match the go.mod inclusion pattern) but will need their BUILD.bazel to have the appropriate rules for visibility setup
 
-	fmt.Printf("Generating rules for go.mod files in %s\n", args.Dir)
+	fmt.Printf("considering  %s\n", args.Dir)
 	for _, file := range args.RegularFiles {
-		fmt.Printf("  - file: %s\n", file)
-		if strings.HasSuffix(file, "go.mod") {
-			srcs = append(srcs, file)
+		if file == "go.mod" {
+			hasGoMod = true
+			break
 		}
 	}
 
 	var res language.GenerateResult
-	if len(srcs) > 0 {
+
+	if hasGoMod {
 		r := rule.NewRule("go_mod", args.Rel)
 		r.SetAttr("go_mod", ":go.mod")
-		r.SetAttr("srcs", srcs)
+		r.SetAttr("srcs", []string{})
 		r.SetAttr("module_path", args.Rel)
 		res.Gen = append(res.Gen, r)
-		res.Imports = append(res.Imports, nil)
-		fmt.Printf("Bingo! %s %v\n", args.Dir, r)
+		res.Imports = append(res.Imports, []resolve.ImportSpec{})
+		fmt.Printf("- Bingo! updating: %s/BUILD.bazel \n  %v\n", args.Dir, r)
 	}
+
 	return res
 }
 
 // Configure is a no-op for this simple extension.
-func (*GoMod) Configure(c *config.Config, rel string, f *rule.File) {}
+func (*GoMod) Configure(c *config.Config, rel string, f *rule.File) {
+	// ... your logic ...
+}
 
 // Imports is a no-op since we're not resolving dependencies.
 func (*GoMod) Imports(c *config.Config, r *rule.Rule, f *rule.File) []resolve.ImportSpec {
@@ -85,8 +88,11 @@ func (*GoMod) Imports(c *config.Config, r *rule.Rule, f *rule.File) []resolve.Im
 }
 
 // Other methods required by the interface (stubs for simplicity).
-func (*GoMod) Fix(c *config.Config, f *rule.File) {}
-func (*GoMod) KnownDirectives() []string          { return nil }
+func (*GoMod) Fix(c *config.Config, f *rule.File) {
+	// ... your logic ...
+}
+
+func (*GoMod) KnownDirectives() []string { return nil }
 func (*GoMod) Resolve(c *config.Config, ix *resolve.RuleIndex, rc *repo.RemoteCache, r *rule.Rule, imports interface{}, from label.Label) {
 }
 
---
+++ foo/bar/go.mod
@@ -0,0 +1,3 @@
+module github.com/stefanpenner/bazel-go-mod-experiment/tools/gazelle_go_mod/foo/bar
+
+go 1.23.3
